f141e698eb7a5dc97ef6dbe9a94dabdc549af658
Zakk <zachary.fleischmann@gmail.com>
2015-04-21 14:55:06 -0400

    finish create

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -6,31 +6,17 @@ class ResponsesController < ApplicationController
   end
 
   def show
-    # Quote.find()
-
     @response = Response.find(params[:id])
-
-    # redirect_to('responses/show')
-
-    @response_emotion = Emotion.find(@response.emoji_id)
-
-    # binding.pry
-  end
-
-  def new
-
-    @user_selection = Response.find(params[:id])
   end
 
   def new
     @quote = Quote.all.sample
-    @response = Response.new(quote: @quote)
+    @response = Response.new
     @emotions = Emotion.all
-    binding.pry
   end
 
   def create
-    @emotional_response = Response.new(params[:emoji_id])
+    @emotional_response = Response.new(response_params)
 
     if @emotional_response.save
       redirect_to @emotional_response
@@ -39,13 +25,8 @@ class ResponsesController < ApplicationController
     end
   end
 
-# private
-#
-#   def response_params
-#     params.require(:response).permit(:)
-#   end
 private
   def response_params
-    params.require[:response].permit(:emoji_id)
+    params.require(:response).permit(:emoji_id, :quote_id)
   end
 end
