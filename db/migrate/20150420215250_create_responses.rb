36de38aa4de1e05059bcad91731f4d5348303c6d
Matt Wuertz <mattwuertz@gmail.com>
2015-04-21 09:29:33 -0400

    commit migration files

--- db/migrate/20150420215250_create_responses.rb
+++ db/migrate/20150420215250_create_responses.rb
@@ -1,7 +1,11 @@
 class CreateResponses < ActiveRecord::Migration
   def change
     create_table :responses do |t|
-
+      t.belongs_to :quote
+      t.has_one :emoji_id
+      t.string :zipcode
+      t.sting :gender
+      t.string :poli_orientation
       t.timestamps null: false
     end
   end
--- db/migrate/20150420215256_create_emotions.rb
+++ db/migrate/20150420215256_create_emotions.rb
@@ -1,7 +1,8 @@
 class CreateEmotions < ActiveRecord::Migration
   def change
     create_table :emotions do |t|
-
+      t.string :title
+      t.text :url
       t.timestamps null: false
     end
   end
