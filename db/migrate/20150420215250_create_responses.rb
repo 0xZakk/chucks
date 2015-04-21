59500986db1d45ed911654c9e9f9ce21392e61a9
Zakk <zachary.fleischmann@gmail.com>
2015-04-21 09:44:23 -0400

    fix migration file

--- db/migrate/20150420215250_create_responses.rb
+++ db/migrate/20150420215250_create_responses.rb
@@ -2,9 +2,9 @@ class CreateResponses < ActiveRecord::Migration
   def change
     create_table :responses do |t|
       t.belongs_to :quote
-      t.has_one :emoji_id
+      t.string :emoji_id
       t.string :zipcode
-      t.sting :gender
+      t.string :gender
       t.string :poli_orientation
       t.timestamps null: false
     end
