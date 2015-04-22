803880d6ff13b7347d724f4a7fc72ccaaf2e7445
Zakk <zachary.fleischmann@gmail.com>
2015-04-22 10:17:50 -0400

    change emoji_id header to emotion to test AR relations

--- db/migrate/20150420215250_create_responses.rb
+++ db/migrate/20150420215250_create_responses.rb
@@ -2,7 +2,7 @@ class CreateResponses < ActiveRecord::Migration
   def change
     create_table :responses do |t|
       t.belongs_to :quote
-      t.integer :emoji_id
+      t.integer :emotion
       t.string :zipcode
       t.string :gender
       t.string :poli_orientation
